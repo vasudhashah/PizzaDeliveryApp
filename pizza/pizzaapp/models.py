f4c0c3df5d41ac809f8059b03d96afbd545ec02e
Vasudha Shah (VS) <vasudhashahbpr@gmail.com>
2020-12-22 15:50:17 +0530

    Add files via upload

---
+++ pizza/pizzaapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class PizzaModel(models.Model):
+    name = models.CharField(max_length = 10)
+    price = models.CharField(max_length = 10)
+
+class CustomerModel(models.Model):
+    userid = models.CharField(max_length = 10)
+    phoneno = models.CharField(max_length = 10)
+
+class OrderModel(models.Model):
+    username = models.CharField(max_length = 10)
+    phoneno = models.CharField(max_length = 10)
+    address = models.CharField(max_length = 10)
+    orderitems = models.CharField(max_length = 10)
+    status = models.CharField(max_length = 10)
